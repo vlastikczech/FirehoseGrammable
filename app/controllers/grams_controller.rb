44221f35aca1ad6b67f82b379ad11ceb8def6d5b
vlastik <vlastikczech1@gmail.com>
2018-04-08 09:55:33 -0700

    Added gram validations

---
+++ app/controllers/grams_controller.rb
@@ -0,0 +1,26 @@
+class GramsController < ApplicationController
+  def new
+    @gram = Gram.new
+  end
+
+
+  def create
+
+    @gram = Gram.create(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  def index
+  end
+
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
+  end
+end
