bda52d1113882eb58a224dfd60d11c9b762e3cff
vlastik <vlastikczech1@gmail.com>
2018-04-10 13:02:19 -0700

    Created test cases for the gram detail page

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -5,7 +5,12 @@ class GramsController < ApplicationController
     @gram = Gram.new
   end
 
-
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      render plain: 'Not Found', status: :not_found
+    end
+  end
   def create
 
     @gram = current_user.grams.create(gram_params)
